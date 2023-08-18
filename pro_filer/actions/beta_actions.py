92094313ae0bc888e490494a4b94f9757a30379d
rubimeryka <erykarubim13@gmail.com>
2023-08-18 15:18:34 -0300

    1- Elimine o(s) bug(s) da função show_deepest_file

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -5,7 +5,8 @@ def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(context["all_files"],
+                           key=lambda path: len(path.split('/')))
         print(f"Deepest file: {deepest_file}")
 
 
@@ -19,8 +20,8 @@ def find_file_by_name(context, search_term, case_sensitive=True):
         file_name = path.split("/")[-1]
 
         if not case_sensitive:
-            file_name.lower()
-            search_term.lower()
+            file_name = file_name.lower()
+            search_term = search_term.lower()
 
         if search_term in file_name:
             found_files.append(path)
